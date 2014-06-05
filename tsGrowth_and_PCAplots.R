33866b23131529b5cc47ba0ac3c9794fd5df4dd6
pkelly13 <pkelly13@nd.edu>
2014-06-05 14:58:20 -0400

    added a script that looks at similarity of FA profiles in seston,  birds, and boli across sites and also explores predictors of tree swallow nestling growth

---
+++ tsGrowth_and_PCAplots.R
@@ -0,0 +1,71 @@
+#This script looks at the differences in tree swallow chick fatty acids across sites and makes comparisons to bolus fatty acids, bug FAs, and seston FAs - script looks at seasonal trends in PUFAs in the seston, in the bugs and in birds.  Also does NMDS to see if bugs, primary production, and birds all have similar FA profiles
+#PTK 24 May 2014
+
+library(vegan)
+
+#load all fatty acid data
+#load TS chick FA data
+setwd('~/bugEmergenceProject')
+chick.fa<-read.csv('tsFAs.csv')
+
+#load bolus FAs
+bolus.fa<-read.csv('bolusFAs.csv')
+
+#load bug FAs
+bug.fa<-read.csv('bugFAs_ug_mg.csv')
+
+#load seston FAs
+ses.fa<-read.csv('SES63_FAs.csv')
+
+#load chick growth data
+chickGrowth<-read.csv('chickGrowth_25Nov2013.csv')
+
+#pcoa with seston data
+#This looks at how the FA profiles across all sites and time points are different from each other
+ses<-ses.fa[,6:52]
+ses<-as.matrix(ses)
+
+ses.dist<-vegdist(ses,method='bray')
+ses.pcoa<-cmdscale(ses.dist)
+
+plot(ses.pcoa,cex=0,xlab='PCA1',ylab='PCA2')
+text(ses.pcoa[,1],ses.pcoa[,2],ses.fa[,2]) #It looks like there is strong grouping by river (ILR v UMR v Em.)
+
+anosim(ses.dist,grouping=ses.fa[,2]) #sig. = 0.001
+
+#Now try this with bird FA data
+chickFA<-chick.fa[,as.numeric(c(17:63))]
+chickFA<-as.matrix(chickFA)
+
+chick.dist<-vegdist(chickFA,method='bray')
+chick.pcoa<-cmdscale(chick.dist)
+
+plot(chick.pcoa,cex=0,xlab='PCA1',ylab='PCA2') #No real grouping by site
+text(chick.pcoa[,1],chick.pcoa[,2],chick.fa$Site_Abbrev)
+
+summary(anosim(chick.dist,grouping=chick.fa$Site_Abbrev)) #significance = 0.001
+
+#PCA with bolus data
+bolusFA<-as.matrix(bolus.fa[,6:48])
+
+bolus.dist<-vegdist(bolusFA)
+bolus.pcoa<-cmdscale(bolus.dist)
+
+plot(bolus.pcoa,cex=0)
+text(bolus.pcoa[,1],bolus.pcoa[,2],bolus.fa$Site_Abbrev)
+
+#Compare chick growth to fatty acids in bolus as well as in livers and seston FA concentration
+chick.growth<-tapply(chickGrowth$Growth..g.d.,chickGrowth$Site_Abreviation,mean,na.rm=T)[-1] #calculate mean chick growth
+
+avgBolus.pufa<-tapply(bolus.fa$PUFA,bolus.fa$Site_Abbrev,mean,na.rm=T)
+plot(avgBolus.pufa,chick.growth)
+summary(lm(chick.growth~avgBolus.pufa)) #r2=0.89 p=0.007
+
+avgChick.pufa<-tapply(chick.fa$PUFA,chick.fa$Site_Abbrev,mean,na.rm=T)
+plot(avgChick.pufa,chick.growth) #no relationship
+
+avgSes.pufa<-tapply(ses.fa$PUFA,ses.fa$Site_Abbrev,mean,na.rm=T)
+plot(avgSes.pufa,chick.growth)
+summary(lm(chick.growth~avgSes.pufa)) #r2=0.43 p=0.154 slope = -0.0088
+
+#Story so far -> chick growth influenced strongly by PUFA availability, but PUFA availability in aquatic primary production or seston does not seem to be a driver of that availability in forgaing.  PUFAs in the diet moreso a function of the taxonomic composition of the prey, and the availaility of "high PUFA" taxa compared to lower PUFA taxa (i.e. mainly brachyceran diptera) - therefore fatty acid characteristics of a subsidy appear to be vitally important for tree swallow nestling growth.
